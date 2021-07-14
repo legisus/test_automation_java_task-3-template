11f65e31af582db3988bde1650be69a45edd8635
Mykola <legisus@gmail.com>
2021-07-14 21:53:52 +0200

    2.00

--- src/main/java/com/epam/test/automation/java/practice3/Main.java
+++ src/main/java/com/epam/test/automation/java/practice3/Main.java
@@ -1,6 +1,7 @@
 package com.epam.test.automation.java.practice3;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -14,7 +15,7 @@ public class Main {
 
         IntStream.range(0, arr.length / 2).forEach(i -> {
             int temp = arr[i];
-            if (temp % 2 == 0 && (arr[arr.length - i - 1]) % 2==0) {
+            if (temp % 2 == 0 && (arr[arr.length - i - 1]) % 2 == 0) {
                 arr[i] = arr[arr.length - i - 1];
                 arr[arr.length - i - 1] = temp;
             }
@@ -29,9 +30,30 @@ public class Main {
      * </summary>
      */
     public static int task2(int[] array) {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        int result = Integer.MAX_VALUE;
+        int max_value = 0;
+        int indexOfMaxValue = 0;
+        int lastIndexMaxValue = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (max_value < array[i]) {
+                max_value = array[i];
+                indexOfMaxValue = i+1;
+            }
+            for (int j = i; j < array.length; j++) {
+                if(max_value == array[j]){
+                    lastIndexMaxValue = j+1;
+                }
+
+            }
+
+        }
+        result = lastIndexMaxValue - indexOfMaxValue;
+
+
+        return result;
     }
+
 
     /**
      * <summary>
@@ -44,15 +66,20 @@ public class Main {
     }
 
 //    public static void main(String[] args) {
-//        int[] ints = {10, 5, 3, 4};
-//        System.out.println("Before: 10, 5, 3, 4");
-//        System.out.println(Arrays.toString(task1(ints)));
-//        System.out.println("Before: 100, 2, 3, 4, 5");
-//        int[] ints2 = {100, 2, 3, 4, 5};
-//        System.out.println(Arrays.toString(task1(ints2)));
-//        System.out.println("Before: 100, 2, 3, 45, 33, 8, 4, 54");
-//        int[] ints3 = {100, 2, 3, 45, 33, 8, 4, 54};
-//        System.out.println(Arrays.toString(task1(ints3)));
+//        int[] ints = {4, 100, 3, 4};
+//        System.out.println(task2(ints));
+//
+//
+//        int[] ints2 = {5, 50, 50, 4, 5};
+//        System.out.println(task2(ints2));
+//
+//        int[] ints3 = {5, 350, 350, 4, 350};
+//        System.out.println(task2(ints3));
+//
+//        int[] ints4 = {10, 10, 10, 10, 10};
+//        System.out.println(task2(ints4));
+//
+//
 //    }
 
 }
